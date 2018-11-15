77fb102ce12af7129126f23d28cf0989f7759db0
markand4 <35792526+markand4@users.noreply.github.com>
2018-11-15 00:51:46 -0700

    ys

--- Node.java
+++ Node.java
@@ -4,17 +4,20 @@ public class Node {
 	
 	private String activityName;
 	private int duration;
-	private ArrayList<String> dependencies = new ArrayList<String>();
+	private ArrayList<String> parents = new ArrayList<String>();
+	private boolean end;
 	
 	public Node() {
 		this.activityName = "ExampleActivity";
 		this.duration = 1;
-		this.dependencies.add("null");
+		this.parents.add("null");
+		this.end = false;
 	}
-	public Node(String activityName, int duration, ArrayList<String> dependencies) {
+	public Node(String activityName, int duration, ArrayList<String> parents) {
 		this.activityName = activityName;
 		this.duration = duration;
-		this.dependencies = (ArrayList<String>) dependencies.clone();
+		this.parents = (ArrayList<String>) parents.clone();
+		this.end = false;
 	}
 	public String getActivityName() {
 		return activityName;
@@ -28,17 +31,26 @@ public class Node {
 	public void setDuration(int duration) {
 		this.duration = duration;
 	}
-	public ArrayList<String> getDependencies() {
-		return dependencies;
+	public ArrayList<String> getParents() {
+		return parents;
 	}
-	public void setDependencies(ArrayList<String> dependencies) {
-		for(int i=0;i<dependencies.size();i++) {
-			this.dependencies.add(dependencies.get(i));
+	public void setParents(ArrayList<String> parents) {
+		for(int i=0;i<parents.size();i++) {
+			this.parents.add(parents.get(i));
 		}
 	}
+	public boolean getEnd() {
+		return end;
+	}
+	public void setEnd( ArrayList<Node> nodes  ){
+        for( Node node : nodes ) {
+            if( node.parents.contains( this.activityName ) ) return;
+        }
+        this.end = true;
+    }
 	@Override
 	public String toString() {
 		return "Node [activityName=" + activityName + ", duration=" + duration
-				+ ", dependencies=" + dependencies + "]";
+				+ ", parents=" + parents + ", end=" + end + "]";
 	}
 }
